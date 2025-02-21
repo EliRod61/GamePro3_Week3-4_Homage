a9125ad8a4c53bd6240c2b045c915e06ad456f35
WittlyEli <162608185+EliRod61@users.noreply.github.com>
2025-02-21 11:54:44 -0800

    Ghosts now patrol the map

--- HomageGame_Pro3/Assets/Scripts/GhostEnemy.cs
+++ HomageGame_Pro3/Assets/Scripts/GhostEnemy.cs
@@ -2,13 +2,18 @@ using UnityEngine;
 
 public class GhostEnemy : MonoBehaviour
 {
+    public Transform[] points;
+    public float speed;
     public int enemyHealth = 25;
+
+    int currentPos;
     int health;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         health = enemyHealth;
+        currentPos = 0;
     }
 
     // Update is called once per frame
@@ -17,6 +22,15 @@ public class GhostEnemy : MonoBehaviour
         if (health <= 0)
         {
             Destroy(gameObject);
+        }
+
+        if (transform.position != points[currentPos].position)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, points[currentPos].position, speed * Time.deltaTime);
+        }
+        else
+        {
+            currentPos = (currentPos + 1) % points.Length;
         }
     }
     public void TakeDamage()
